bc656db9b6a9660efe7e03a1d0935d197ac9b0dd
Pinguino25 <andrew.hignett@hotmail.co.uk>
2020-10-13 21:00:35 +0100

    Working retrieving webcam

--- FireMatchTracker/matchTracker.cu
+++ FireMatchTracker/matchTracker.cu
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
 using namespace std;
 #include <opencv2/core.hpp>
@@ -29,5 +31,69 @@ int main()
 	DeviceInfo _deviceInfo;
 	bool _isd_evice_compatible = _deviceInfo.isCompatible();
 	cout << "CUDA Device(s) Compatible: " << _isd_evice_compatible << endl;
+
+	Mat frame;
+	VideoCapture cam;// = VideoCapture(0, CAP_DSHOW);
+	cam.open(0);// CAP_DSHOW);
+	if (!cam.isOpened()) {
+		cerr << "ERROR Unable to open camera\n";
+		return -1;
+	}
+	//cam.read(frame);
+	//if (frame.empty()) {
+	//	cerr << "ERROR Blank frame\n";
+	//	return -1;
+	//}
+	destroyAllWindows();
+
+
+	//working code that opens and displays the webcam
+	VideoCapture cap(0);
+	while (1)
+	{
+		Mat image;
+		cap >> image;
+		if (!image.data) break;
+		if (waitKey(30) >= 0) break;
+
+		imshow("test", image);
+		waitKey(1);
+	}
+
+
+	//Old basic CUDA code sample
+	//--- INITIALIZE VIDEOCAPTURE
+	//VideoCapture cap;
+	// open the default camera using default API
+	// cap.open(0);
+	// OR advance usage: select any API backend
+	//int deviceID = 0;             // 0 = open default camera
+	//int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+	// open selected camera using selected API
+	//cap.open(deviceID, apiID);
+	// check if we succeeded
+	/*if (!cap.isOpened()) {
+		cerr << "ERROR! Unable to open camera\n";
+		return -1;
+	}
+	
+	//--- GRAB AND WRITE LOOP
+	cout << "Start grabbing" << endl
+		<< "Press any key to terminate" << endl;
+	for (;;)
+	{
+		// wait for a new frame from camera and store it into 'frame'
+		cap.read(frame);
+		// check if we succeeded
+		if (frame.empty()) {
+			cerr << "ERROR! blank frame grabbed\n";
+			break;
+		}
+		// show live and wait for a key with timeout long enough to show images
+		imshow("Live", frame);
+		if (waitKey(5) >= 0)
+			break;
+	}*/
+
 	return 0;
 }
