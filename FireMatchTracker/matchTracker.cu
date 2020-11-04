4c28e712037d0470e039de5705ba1a2e9f62d04f
Pinguino25 <andrew.hignett@hotmail.co.uk>
2020-11-04 14:44:56 +0000

    Threads and blocks added

--- FireMatchTracker/matchTracker.cu
+++ FireMatchTracker/matchTracker.cu
@@ -2,7 +2,7 @@
 #include <device_launch_parameters.h>
 #include "matchTracker.h"
 
-__global__ void trackKernel(int *c, const int *a, const int *b)
+__global__ void trackKernel(int *out, Mat frame, int x, int y)
 {
 	//detect end
 	//determine orientation
@@ -10,11 +10,23 @@ __global__ void trackKernel(int *c, const int *a, const int *b)
 	//have internal representation of it's position in 3D
 	//draw particles in 3D space
 	//move particles with physics based on the match's movement
+
+	int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+	if (threadId < x * y)
+	{
+		int thisX = ;
+		int thisY = ;
+		Vec3b pixel = frame.at<Vec3b>(thisX, thisY);
+	}
 }
 
 void track(Mat frame) {
 	Vec3b pixel = frame.at<Vec3b>(0, 0);
 	//BGR pixel values
 	printf("%d %d %d\n", pixel[0], pixel[1], pixel[2]);
+	int threadCount = 1024;
+	int x = 1280;
+	int y = 720;
+	int blocks = (x * y - 1) / threadCount + 1;
 	//trackKernel<<<, >>>();
 }
